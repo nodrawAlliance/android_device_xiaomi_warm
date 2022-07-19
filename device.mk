7d6f0ec1a909d295c611801c5ed10fca6cb572bd
Jake Weinstein <jake@aospa.co>
2022-07-19 14:25:47 -0500

    warm: Remove framework_manifest
    Telephony component does this.

--- device.mk
+++ device.mk
@@ -352,8 +352,6 @@ AB_OTA_POSTINSTALL_CONFIG += \
 QTI_CAMERA_PROVIDER_SERVICE := 2.7
 QTI_CAMERA_AON_SERVICE := 1.2
 
-DEVICE_FRAMEWORK_MANIFEST_FILE := device/xiaomi/warm/framework_manifest.xml
-
 # Enable compilation of image_generation_tool
 TARGET_USES_IMAGE_GEN_TOOL := true
 
