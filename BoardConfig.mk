9f1a430d37c076e26dec41f4e0b6d59359a4ea70
Jake Weinstein <jake@aospa.co>
2022-07-19 16:44:38 -0500

    warm: Use fstab.qcom as recovery.fstab
    Why duplicate?

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,7 +55,7 @@ BOARD_EXT4_SHARE_DUP_BLOCKS := true
 TARGET_NO_RECOVERY := true
 # Defines for enabling A/B builds
 AB_OTA_UPDATER := true
-TARGET_RECOVERY_FSTAB := device/xiaomi/warm/recovery.fstab
+TARGET_RECOVERY_FSTAB := device/xiaomi/warm/default/fstab_AB_dynamic_partition.qti
 
 BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
 BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
