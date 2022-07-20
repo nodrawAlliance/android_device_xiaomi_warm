c31c5829275af3252f8ae4016e98ca0f00bf0652
Jake Weinstein <jake@aospa.co>
2022-07-19 21:20:32 -0500

    warm: Enable permissive SELinux for debugging

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,6 +125,8 @@ endif
 BOARD_KERNEL_CMDLINE += $(file < device/qcom/$(TARGET_BOARD_PLATFORM)-kernel/extra_cmdline)
 BOARD_BOOTCONFIG += $(file < device/qcom/$(TARGET_BOARD_PLATFORM)-kernel/extra_bootconfig)
 
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
